48e526fdddc1c50e0efb24ccaee0c1d9f7443219
Zony Yu <zony249@gmail.com>
2020-10-04 20:33:11 -0600

    added makefile

--- freq.py
+++ freq.py
@@ -1,19 +1,26 @@
 #--------------------------------------------
-#   Name: 
-#   ID: 
+#   Name: Zong Lin Yu 
+#   ID: 1614934
 #   CMPUT 274, Fall 2020
 #
 #   Weekly Exercise 3: Word Frequency
 #-------------------------------------------- 
-# FREQ TEMPLATE: ADD YOUR INFORMATION TO ABOVE
-
-# You must determine how to structure your solution.
-# Create your functions here and call them from under
-# if __name__ == "__main__"!
 
 import sys, os
 
 def safe_open(filename):
+    """ Safely opens a file, checking if the file exists.
+    If the file does not exist, raises FileNotFoundError.
+
+    Argument:
+        filename (str): A string representing
+            the path of the file.
+
+    Returns:
+        f (file object): A file object where the permission
+            is set to "r".
+        
+    """
     if os.path.isfile(filename):
         f = open(filename, "r")
         return (f)
@@ -21,7 +28,18 @@ def safe_open(filename):
         raise FileNotFoundError()
 
 def count_words(file_obj):
-    
+    """ Creates a dictionary that stores each unique word
+    in the file object as well as its occurance. 
+
+    Arguments:
+        file_obj (file object): A file object
+
+    Returns:
+        words (dict): A dictionary that contains every
+            unique word as well as the number
+            of times that word occurs.
+
+    """
     cFlag = True
     words = {}
     while cFlag:
@@ -38,9 +56,27 @@ def count_words(file_obj):
     return words
 
 def hflag():
-    print("help")
+    h_text = "Usage: python3 freq.py <argument> \n"
+    h_text += "    <argument>: \n"
+    h_text += "        <filename>: the path of a file \n"
+    h_text += "        -h or --help: display this help message\n"
+    print(h_text)
 
 def print_freq_tab(dictionary, filename):
+    """ Prints the frequency table in the form 
+    <word> <occurances> <frequency fraction>
+    where:
+        <word>: a unique word contained in the dictionary
+        <occurances>: the number of times <word> occurs
+        <frequency fraction>: <occurances> /(total number of words)
+
+    Arguments:
+        dictionary (dict): A dictionary where the keys are the
+            words and the values are the number of occurances
+
+        filename (str): The file name of the file that was opened.
+            This program saves the output as <filename>.out.
+    """
     list_sorted_keys = sorted(dictionary.keys())
     total_count = sum(dictionary.values())
     
@@ -51,7 +87,7 @@ def print_freq_tab(dictionary, filename):
         key = i
         value = dictionary[i]
         freq = round(dictionary[i]/total_count, 3)
-        print('%-15s%-15s%-15s' % (key, value, freq), file=f_out)
+        print('%s %s %s' % (key, value, freq), file=f_out)
     f_out.close()
     
 def main():
@@ -63,7 +99,7 @@ def main():
             return 0
         filename = sys.argv[1]
         f = safe_open(filename)
-    except IndexError:
+    except IndexError: # if sys.argv doesn't have index [1]
         print("Please include the name of your file as shown: ")
         print("python3 freq.py <filename>")
         return -1
@@ -78,6 +114,9 @@ def main():
 
     words = count_words(f)
     print_freq_tab(words, filename)
+    f.close()
+    return 0
+
 
 if __name__ == "__main__":
     # Any code indented under this line will only be run
